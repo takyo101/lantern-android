8630affc1a9c764449ea47185605f6a2c5d9cc2d
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-01-22 12:50:09 -0600

    Making proxy server spawn within a goroutine.

--- libflashlight/bindings/main.go
+++ libflashlight/bindings/main.go
@@ -5,18 +5,30 @@ package flashlight
 
 import (
 	"github.com/getlantern/lantern-android/client"
+	"strings"
 )
 
 var DefaultClient *client.Client
 
 // StopClientProxy stops the proxy.
 func StopClientProxy() error {
-	return DefaultClient.Stop()
+	DefaultClient.Stop()
+	return nil
 }
 
-// RunClientProxy creates a new client at the given address. If an active
-// client is found it kill the client before starting a new one.
-func RunClientProxy(listenAddr string) (err error) {
+// RunClientProxy creates a new client at the given address.
+func RunClientProxy(listenAddr string) error {
+
 	DefaultClient = client.NewClient(listenAddr)
+
+	go func() {
+		var err error
+		if err = DefaultClient.ListenAndServe(); err != nil {
+			// Error is not exported: https://golang.org/src/net/net.go#L284
+			if !strings.Contains(err.Error(), "use of closed network connection") {
+				panic(err.Error())
+			}
+		}
+	}()
 	return nil
 }

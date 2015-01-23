b79b6922a164e03f627f1a3ebcad2026492f79d0
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-01-23 14:38:37 -0600

    Planning test for HTTPs URIs.

--- client/client.go
+++ client/client.go
@@ -14,6 +14,7 @@ import (
 
 const (
 	HttpConnectMethod = "CONNECT"
+	XFlashlightQOS    = "X-Flashlight-QOS"
 )
 
 // Client is a HTTP proxy that accepts connections from local programs and
--- client/client_test.go
+++ client/client_test.go
@@ -1,7 +1,7 @@
 package client
 
 import (
-	"errors"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -13,36 +13,49 @@ import (
 
 const listenProxyAddr = "127.0.0.1:9997"
 
-const expectedBody = "Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"
+var testURLs = map[string][]byte{
+	"http://www.google.com/humans.txt": []byte("Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"),
+	// TODO: This is not working, we actually need to implement a CONNECT request to proxy HTTPs traffic.
+	// "https://www.google.com/humans.txt": []byte("Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"),
+}
 
-func testReverseProxy() error {
+func testReverseProxy(uri string, expectedContent []byte) (err error) {
 	var req *http.Request
+	var u *url.URL
+
+	if u, err = url.Parse(uri); err != nil {
+		return err
+	}
+
+	port := 80
+
+	if u.Scheme == "https" {
+		// TODO: implement a CONNECT request.
+	}
 
 	req = &http.Request{
 		Method: "GET",
 		URL: &url.URL{
-			Scheme: "http",
-			Host:   "www.google.com",
-			Path:   "http://www.google.com/humans.txt",
+			Scheme: u.Scheme,
+			Host:   u.Host,
+			Path:   uri,
 		},
 		ProtoMajor: 1,
 		ProtoMinor: 1,
 		Header: http.Header{
-			"Host": {"www.google.com:80"},
+			"Host": {fmt.Sprintf("%s:%d", u.Host, port)},
 		},
 	}
 
 	client := &http.Client{
 		Transport: &http.Transport{
 			Dial: func(n, a string) (net.Conn, error) {
-				//return net.Dial("tcp", "127.0.0.1:9898")
 				return net.Dial("tcp", listenProxyAddr)
 			},
 		},
 	}
 
 	var res *http.Response
-	var err error
 
 	if res, err = client.Do(req); err != nil {
 		return err
@@ -54,8 +67,8 @@ func testReverseProxy() error {
 
 	fmt.Printf(string(buf))
 
-	if string(buf) != expectedBody {
-		return errors.New("Expecting another response.")
+	if bytes.Equal(buf, expectedContent) == false {
+		return fmt.Errorf("The response we've got from %s differs from what we expected.", uri)
 	}
 
 	return nil
@@ -74,10 +87,10 @@ func TestListenAndServeStop(t *testing.T) {
 	c.Stop()
 }
 
-func TestListenAndServeSpawn(t *testing.T) {
+func TestListenAndServeAgain(t *testing.T) {
 
 	go func() {
-		c := NewClient(":9997")
+		c := NewClient(listenProxyAddr)
 		var err error
 		if err = c.ListenAndServe(); err != nil {
 			t.Fatal(err)
@@ -87,8 +100,9 @@ func TestListenAndServeSpawn(t *testing.T) {
 }
 
 func TestListenAndServeProxy(t *testing.T) {
-	err := testReverseProxy()
-	if err != nil {
-		t.Fatal(err)
+	for uri, expectedContent := range testURLs {
+		if err := testReverseProxy(uri, expectedContent); err != nil {
+			t.Fatal(err)
+		}
 	}
 }
